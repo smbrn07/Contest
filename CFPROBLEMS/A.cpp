5782a37106aa8018eda5f0eaec644dc7ba3f1cb6
smbrn07 <buihoangminhsmbrn07@gmail.com>
2024-08-24 15:50:22 +0700

    upgrade

--- CFPROBLEMS/A.cpp
+++ CFPROBLEMS/A.cpp
@@ -1,6 +1,65 @@
 #include "bits/stdc++.h"
-
 using namespace std;
+
+const int mod = 1e9 + 7;
+const int BUF_SZ = 1 << 15;
+
+inline namespace Input {
+char buf[BUF_SZ];
+int pos;
+int len;
+char next_char() {
+   if (pos == len) {
+      pos = 0;
+      len = (int)fread(buf, 1, BUF_SZ, stdin);
+      if (!len) { return EOF; }
+   }
+   return buf[pos++];
+}
+
+int read_int() {
+   int x;
+   char ch;
+   int sgn = 1;
+   while (!isdigit(ch = next_char())) {
+      if (ch == '-') { sgn *= -1; }
+   }
+   x = ch - '0';
+   while (isdigit(ch = next_char())) { x = x * 10 + (ch - '0'); }
+   return x * sgn;
+}
+}
+
+inline namespace Output {
+char buf[BUF_SZ];
+int pos;
+
+void flush_out() {
+   fwrite(buf, 1, pos, stdout);
+   pos = 0;
+}
+
+void write_char(char c) {
+   if (pos == BUF_SZ) { flush_out(); }
+   buf[pos++] = c;
+}
+
+void write_int(int x) {
+   static char num_buf[100];
+   if (x < 0) {
+      write_char('-');
+      x *= -1;
+   }
+   int len = 0;
+   for (; x >= 10; x /= 10) { num_buf[len++] = (char)('0' + (x % 10)); }
+   write_char((char)('0' + x));
+   while (len) { write_char(num_buf[--len]); }
+   write_char('\n');
+}
+
+// auto-flush output when program exits
+void init_output() { assert(atexit(flush_out) == 0); }
+}
 
 typedef long long ll;
 
@@ -10,28 +69,11 @@ typedef long long ll;
 #define pb push_back
 #define sz(a) int(a.size())
 #define allin(a) begin(a), end(a)
-#define file(name) freopen(name".in","r",stdin); freopen(name".out","w",stdout);
-
-const int mod = 1e9+7;
-const int nmax = 1e5+7;
 
 signed main() {
-   cin.tie(nullptr)->sync_with_stdio(false);   
-   int tt; cin >> tt;
-   while (tt--) {
-   	int n; string s; cin >> n >> s;
-   	vector<int> d(4, 0);
-   	int cnt = 0;
-   	for (char i : s) {
-   		if (i=='?') ++cnt;
-   		else ++d[i-'A'];
-   	}
-   	int minv = min({d[0], d[1], d[2], d[3]});
-   	int ans = 0;
-   	for (int i = 0; i < 4; ++i) {
-   		ans += min(d[i], n);
-   	}
-   	cout << ans << endl;
-   }
+   init_output();
+   // cin ~ read_int();
+   // cout ~ write_int();
+
    return 0;
 }
